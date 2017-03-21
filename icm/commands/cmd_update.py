1acba30aa3e969fa5e87f668a746775c1b1a2b2c
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2017-03-21 10:00:37 +0100

    Refactor "update command"

--- icm/commands/cmd_update.py
+++ icm/commands/cmd_update.py
@@ -16,39 +16,38 @@ def update():
     click.secho('Update the collection', fg='cyan')
 
     if validate_collection():
-
         # Update README.md
-        newdata = getdocs()
-        if os.path.exists('README.md'):
-            with open('README.md', 'r') as data:
-                if data.read() == newdata:
-                    click.secho('`README.md` file already up-to-date',
-                                fg='yellow')
-                else:
-                    if click.confirm('The `README.md` file has changes.\n'
-                                     'Do you want to replace it?'):
-                        update_readme(newdata)
-                        click.secho('`README.md` updated',
-                                    fg='green')
-                    else:
-                        click.secho('`README.md` not updated',
-                                    fg='red')
-
-        else:
-            update_readme(newdata)
-            click.secho('`README.md` created',
-                        fg='green')
-
+        update_file('README.md', getdocs())
         # Update locale/translation.js
-        # TODO
-
+        update_file('locale/translation.js', gettext())
     else:
         click.secho('\nThe collection is not valid :(', fg='red')
 
 
-def update_readme(data):
-    with open('README.md', 'w') as f:
-        f.write(data)
+def update_file(dest, data):
+    if os.path.exists(dest):
+        with open(dest, 'r') as f:
+            if f.read() == data:
+                click.secho('`{}` file already up-to-date'.format(dest),
+                            fg='yellow')
+            else:
+                if click.confirm('The `{}` file has changes.\n'
+                                 'Do you want to replace it?'.format(dest)):
+                    with open(dest, 'w') as f:
+                        f.write(data)
+                    click.secho('`{}` updated'.format(dest),
+                                fg='green')
+                else:
+                    click.secho('`{}` not updated'.format(dest),
+                                fg='red')
+    else:
+        path = os.path.dirname(dest)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        with open(dest, 'w') as f:
+            f.write(data)
+        click.secho('`{}` created'.format(dest),
+                    fg='green')
 
 
 def getdocs():
@@ -56,4 +55,4 @@ def getdocs():
 
 
 def gettext():
-    pass
+    return '// Translation document of the collection'
