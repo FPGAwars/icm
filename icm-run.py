64994ba1c63eb5606b716e7418500556aaa43a4d
Obijuan <obijuan.cube@gmail.com>
2024-06-16 18:00:46 +0200

    Add Debug configuration

---
+++ icm-run.py
@@ -0,0 +1,23 @@
+#!venv/bin/python
+"""Run icm for debugging"""
+# ------------------------------------------------
+# -- Run icm for debugging
+# -- It is not part of icm (it is not installed).
+# --  It is just a launcher for the developers
+#-------------------------------------------------
+
+import sys
+
+# -- Import the apio entry point
+from icm.__main__ import cli as icm
+
+#-- Run apio!
+try:
+    icm(None)
+
+#-- Apio commands finish with this excepcion
+except SystemExit:
+    print("icm command executed!")
+
+#-- Exit!
+sys.exit()
