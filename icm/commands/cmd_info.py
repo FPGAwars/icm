0d0f53fe0340f303390a2679c7bfc0e61520e7d2
Obijuan <obijuan.cube@gmail.com>
2024-06-16 20:20:40 +0200

    info command: initial implementation

--- icm/commands/cmd_info.py
+++ icm/commands/cmd_info.py
@@ -10,6 +10,10 @@
 
 import platform
 import shutil
+from pathlib import Path
+
+import click
+
 
 # -- Terminal width
 TERMINAL_WIDTH = shutil.get_terminal_size().columns
@@ -17,16 +21,67 @@ TERMINAL_WIDTH = shutil.get_terminal_size().columns
 # -- Horizontal line
 HLINE = "─" * TERMINAL_WIDTH
 
+# -- Home user folder
+HOME = Path.home()
+
+# -- Icestudio HOME dir
+ICESTUDIO_HOME = HOME / ".icestudio"
+
+# -- Icestudio root for collections
+COLLECTIONS = ICESTUDIO_HOME / "collections"
+
 
 def main():
     """ENTRY POINT: Show system information"""
 
+    # --------------------------------
+    # -- Print System information
+    # --------------------------------
+    # -- Header
     print()
-    print("----------------")
-    print("icm info")
-    print("----------------")
-    print("")
+    click.secho(HLINE, fg="green")
+    click.secho("SYSTEM INFORMATION", fg="green")
+    click.secho(HLINE, fg="green")
 
-    print(HLINE)
-    print(platform.uname())
-    print(HLINE)
+    # -- Read system information
+    plat = platform.uname()
+
+    # -- Print it!
+    click.echo(click.style("• Processor: ", fg="green") + f"{plat.processor}")
+    click.echo(click.style("• System: ", fg="green") + f"{plat.system}")
+    click.echo(click.style("• Release: ", fg="green") + f"{plat.release}")
+    click.echo(click.style("• Version: ", fg="green") + f"{plat.version}")
+
+    # ----------------------------
+    # -- System folders
+    # ----------------------------
+    # -- Header
+    print()
+    click.secho(HLINE, fg="yellow")
+    click.secho("FOLDERS", fg="yellow")
+    click.secho(HLINE, fg="yellow")
+
+    # -- Check if all the folders exist or not
+    ok_home = HOME.exists()
+    ok_icestudio = ICESTUDIO_HOME.exists()
+    ok_collections = COLLECTIONS.exists()
+
+    # -- Choose the correct bullet for the folder
+    home_check = "✅ " if ok_home else "❌ "
+    icestudio_check = "✅ " if ok_icestudio else "❌ "
+    collections_check = "✅ " if ok_collections else "❌ "
+
+    # -- Print the information
+    click.echo(home_check + click.style("HOME: ", fg="yellow") + f"{HOME}")
+    click.echo(
+        icestudio_check
+        + click.style("Icestudio: ", fg="yellow")
+        + f"{ICESTUDIO_HOME}"
+    )
+    click.echo(
+        collections_check
+        + click.style("Collections: ", fg="yellow")
+        + f"{COLLECTIONS}"
+    )
+
+    print()
