e10b04452fe84474d8915c072b4fcaa03ac93bbe
Obijuan <obijuan.cube@gmail.com>
2024-06-17 12:50:22 +0200

    icm install: refactoring

--- icm/commands/cmd_install.py
+++ icm/commands/cmd_install.py
@@ -2,20 +2,88 @@
 
 import zipfile
 import os
+from typing import NamedTuple
+from pathlib import Path
+
 import requests
 from tqdm import tqdm
 from icm.commons import commons
 
+
+class Collection(NamedTuple):
+    """Manage collections"""
+
+    #   Oficial icestudio Collection are zip files downloaded
+    #   from https://github.com/FPGAwars/
+
+    #   There are two types or URL:
+
+    #   * Dev URL:  Latest version in the main branch
+    #     https://github.com/FPGAwars/iceK/archive/refs/heads/main.zip
+
+    #   * Version URL:  Specific stable version
+    #     https://github.com/FPGAwars/iceK/archive/refs/tags/v0.1.4.zip
+
+    GITHUB_FPGAWARS = "https://github.com/FPGAwars/"
+    GITHUB_PREFIX = "/archive/refs/"
+    GITHUB_TYPE_DEV = "heads/"
+    GITHUB_TYPE_VER = "tags/"
+
+    def filename(self, version="") -> str:
+        """Return the coleccion filename, according to its version
+         No Path. No url. Just the .zip filename
+
+        * No version given --> assume dev collection. filename = "main.zip"
+        * version given --> filename = "v{version}.zip"
+
+        Ex. col.filename("0.1.4) --> "v0.1.4.zip"
+        """
+
+        # -- Filename for the dev collection
+        name_dev = "main.zip"
+
+        # -- Filename for the version (stable) collection
+        name_ver = f"v{version}.zip"
+
+        # -- Return the name according to the version type
+        return name_ver if version else name_dev
+
+    def abs_filename(self, folders: commons.Folders, version="") -> Path:
+        """Return the absolute path of the collection target filename
+        ex: /home/obijuan/.icestudio/collections/v0.1.4.zip
+        """
+        abs_file = folders.collections / self.filename(version)
+        return abs_file
+
+    def url(self, name: str, version="") -> str:
+        """Return the url of the given collection
+        * name: Collection name (Ex. "iceK")
+        * version (optional) (Ex. 0.1.4)
+
+        It return the url as a string
+        """
+
+        # -- Get the colection filename
+        filename = self.filename(version)
+
+        # -- Get the type string
+        github_type = self.GITHUB_TYPE_VER if version else self.GITHUB_TYPE_DEV
+
+        # -- Build the url
+        url = (
+            f"{self.GITHUB_FPGAWARS}{name}{self.GITHUB_PREFIX}"
+            f"{github_type}{filename}"
+        )
+
+        # -- Return the url
+        return url
+
+
 # -- Information about collections
 icek = {"name": "iceK", "version": "0.1.4"}
 
-# -- Collection file
+# -- Collection filefo
 COLLECTION_FILE = "v"
-
-# -- Template URL
-# -- Full collection download url: PREFIX + NAME + SUFFIX + FILE
-TEMPLATE_URL_PREFIX = "https://github.com/FPGAwars/"
-TEMPLATE_URL_SUFFIX = "/archive/refs/tags/"
 
 
 def main():
@@ -23,21 +91,16 @@ def main():
 
     # -- Get context information
     folders = commons.Folders()
+    colection = Collection()
 
-    print("Install!!!")
+    print()
+    print("Install!!!\n")
 
-    # --- Scafold for the installation of collections
-    # -- Download url:
-    #   https://github.com/FPGAwars/iceK/archive/refs/tags/v0.1.4.zip
-    # -- Build the collection filename
-    filename = f"v{icek['version']}.zip"
-    print(f"Colection file: {filename}")
+    abs_filename = colection.abs_filename(folders, icek["version"])
+    print(f"* File: {abs_filename}")
 
-    absolut_filename = f"{folders.collections}/{filename}"
-    print(f"Absolut filename: {absolut_filename}")
-
-    url = f"{TEMPLATE_URL_PREFIX}{icek['name']}{TEMPLATE_URL_SUFFIX}{filename}"
-    print(f"Url: {url}")
+    url = colection.url(icek["name"], icek["version"])
+    print(f"* Url: {url}")
 
     # -- Download the collection
     # Realizar la solicitud HTTP para obtener el contenido del archivo
@@ -51,11 +114,14 @@ def main():
 
         # Abrir un archivo local con el nombre especificado en
         # modo escritura binaria
-        with open(absolut_filename, "wb") as file:
+        with open(abs_filename, "wb") as file:
 
             # Crear una barra de progreso con tqdm
             with tqdm(
-                total=total_size, unit="B", unit_scale=True, desc=filename
+                total=total_size,
+                unit="B",
+                unit_scale=True,
+                desc="  Downloading",
             ) as pbar:
                 # Iterar sobre el contenido en bloques
                 for chunk in response.iter_content(chunk_size=1024):
@@ -67,7 +133,6 @@ def main():
                         pbar.update(len(chunk))
 
         # shutil.copyfileobj(response.raw, file)
-        print(f"Archivo descargado y guardado como {filename}")
     else:
         print(
             f"Error al descargar el archivo. "
@@ -77,7 +142,7 @@ def main():
     # -- Uncompress the collection
 
     # Nombre del archivo ZIP
-    zip_filename = absolut_filename
+    zip_filename = abs_filename
 
     # Directorio de destino para descomprimir los archivos
     extract_to = folders.collections
@@ -89,7 +154,7 @@ def main():
 
         # Crear una barra de progreso
         with tqdm(
-            total=len(file_list), desc="Descomprimiendo", unit="file"
+            total=len(file_list), desc="  Uncompressing", unit="file"
         ) as pbar:
             # Iterar sobre cada archivo en el archivo ZIP
             for file in file_list:
